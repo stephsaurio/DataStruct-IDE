8b01588763711ba075080353d838153d2ba1d755
stephsaurio <gomezstephany151@gmail.com>
2026-09-02 09:59:48 -0600

    interface

---
+++ src/app/components/token-table/token-table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-token-table',
+  styleUrl: './token-table.css',
+  templateUrl: './token-table.html',
+})
+export class TokenTable {}
